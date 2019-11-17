7c9b0522488fea86b65a3434251fc086f65e06df
Konrad Rokicki <rokickik@janelia.hhmi.org>
2019-11-17 16:25:47 -0500

    create all resources, changed to async/await style

--- lambda-resize/index.js
+++ lambda-resize/index.js
@@ -5,35 +5,43 @@ const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 const sharp = require('sharp');
 const width = 400;
 const height = 300;
-const targetBucket = "krokicki-photos-medium";
+const targetBucket = process.env.OUTPUT_BUCKET
 
-exports.handler = function(event, context, callback) {
+exports.handler = async (event, context) => {
 
-    console.log('Received event:', JSON.stringify(event, null, 2));
-    const bucket = event.Records[0].s3.bucket.name;
-    const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
-    
-    s3.getObject({ Bucket: bucket, Key: key}).promise()
-        .then(data => sharp(data.Body)
-            .resize(width, height)
-            .jpeg()
-            .toBuffer()
-        )
-        .then(buffer => s3.putObject({
-                Body: buffer,
-                Bucket: targetBucket,
-                ContentType: 'image/jpeg',
-                Key: key
-            }).promise()
-        )
-        .then(() => {
-            console.log('Resized into ', targetBucket);
-            callback(null, { statusCode: '204' });
-        })
-        .catch(err => {
-            console.log('Error: ', err);
-            callback(err);
-        })
+    if (!targetBucket) {
+        context.fail('Error: Environment variable OUTPUT_BUCKET missing')
+        return
+    }
 
-    return true;
+    if (event.Records === null) {
+        context.fail('Error: Event has no records.')
+        return
+    }
+
+    try {
+        console.log('Received event:', JSON.stringify(event, null, 2));
+        const bucket = event.Records[0].s3.bucket.name;
+        const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+
+        const origImage = await s3.getObject({ Bucket: bucket, Key: key}).promise()
+        console.log('Got new file: ', key)
+
+        const sizedImage = await sharp(origImage.Body).resize(width, height).jpeg().toBuffer()
+        console.log('Resized image to ', width, 'x', height)
+
+        await s3.putObject({
+            Body: sizedImage,
+            Bucket: targetBucket,
+            ContentType: 'image/jpeg',
+            Key: key
+        }).promise()
+
+        console.log('Resized into ', targetBucket)
+            
+        context.succeed()
+    }
+    catch (err) {
+        context.fail(err)
+    }
 }
