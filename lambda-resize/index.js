096733f28ab84cc28b94d80aacf8c5a6887e9263
Konrad Rokicki <rokickik@janelia.hhmi.org>
2019-11-18 18:28:05 -0500

    add width to resized filename

--- lambda-resize/index.js
+++ lambda-resize/index.js
@@ -3,9 +3,11 @@
 const aws = require('aws-sdk');
 const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 const sharp = require('sharp');
-const width = 400;
-const height = 300;
+const path = require('path');
+
 const targetBucket = process.env.OUTPUT_BUCKET
+const width = Number(process.env.OUTPUT_WIDTH)
+const height = Number(process.env.OUTPUT_HEIGHT)
 
 exports.handler = async (event, context) => {
 
@@ -24,21 +26,23 @@ exports.handler = async (event, context) => {
         const bucket = event.Records[0].s3.bucket.name;
         const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
 
+        console.log('Retrieving s3://', bucket, '/', key)
         const origImage = await s3.getObject({ Bucket: bucket, Key: key}).promise()
-        console.log('Got new file: ', key)
-
+        
         const sizedImage = await sharp(origImage.Body).resize(width, height).jpeg().toBuffer()
         console.log('Resized image to ', width, 'x', height)
+
+        const p = path.parse(key)
+        const targetKey = path.format({ dir:p.dir, name:p.name+"-"+width, ext:p.ext })
+        console.log('Saving to s3://', targetBucket, '/', targetKey)
 
         await s3.putObject({
             Body: sizedImage,
             Bucket: targetBucket,
             ContentType: 'image/jpeg',
-            Key: key
+            Key: targetKey
         }).promise()
 
-        console.log('Resized into ', targetBucket)
-            
         context.succeed()
     }
     catch (err) {
